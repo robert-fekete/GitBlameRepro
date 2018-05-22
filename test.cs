47cec46d29c71a6f28c2c90a9641df8d0acfabe8
rfe <rfe@phaseone.com>
2018-05-22 11:17:18 +0200

    Change 8097

--- test.cs
+++ test.cs
@@ -102,7 +102,7 @@ namespace Runner
             int i94 = 8094;
             int i95 = 8095;
             int i96 = 8096;
-            int i97 = 97;
+            int i97 = 8097;
             int i98 = 98;
             int i99 = 99;
             int i100 = 100;
