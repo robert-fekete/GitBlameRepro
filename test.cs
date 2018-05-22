01c4b00f86405f41fa6c1e810e63b912bb46ad97
rfe <rfe@phaseone.com>
2018-05-22 11:17:58 +0200

    Change 8478

--- test.cs
+++ test.cs
@@ -483,7 +483,7 @@ namespace Runner
             int i475 = 8475;
             int i476 = 8476;
             int i477 = 8477;
-            int i478 = 478;
+            int i478 = 8478;
             int i479 = 479;
             int i480 = 480;
             int i481 = 481;
