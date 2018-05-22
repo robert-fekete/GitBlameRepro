4963a2ee817d862f39ce85dd93074c7c57498258
rfe <rfe@phaseone.com>
2018-05-22 11:18:38 +0200

    Change 8848

--- test.cs
+++ test.cs
@@ -853,7 +853,7 @@ namespace Runner
             int i845 = 8845;
             int i846 = 8846;
             int i847 = 8847;
-            int i848 = 848;
+            int i848 = 8848;
             int i849 = 849;
             int i850 = 850;
             int i851 = 851;
