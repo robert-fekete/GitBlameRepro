a7392bcac94f6cc506d1db67333a5e1b22a79ffc
rfe <rfe@phaseone.com>
2018-05-22 11:19:02 +0200

    Change 9079

--- test.cs
+++ test.cs
@@ -1084,7 +1084,7 @@ namespace Runner
             int i1076 = 9076;
             int i1077 = 9077;
             int i1078 = 9078;
-            int i1079 = 1079;
+            int i1079 = 9079;
             int i1080 = 1080;
             int i1081 = 1081;
             int i1082 = 1082;
