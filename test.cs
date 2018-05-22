8968ceed3a6b9b3d77fdc18620dfdcfec8866c21
rfe <rfe@phaseone.com>
2018-05-22 11:21:10 +0200

    Change 10234

--- test.cs
+++ test.cs
@@ -2239,7 +2239,7 @@ namespace Runner
             int i2231 = 10231;
             int i2232 = 10232;
             int i2233 = 10233;
-            int i2234 = 2234;
+            int i2234 = 10234;
             int i2235 = 2235;
             int i2236 = 2236;
             int i2237 = 2237;
