a1ecd1938f3527bb240cfac5e00fe3779dca62fe
rfe <rfe@phaseone.com>
2018-05-22 11:24:18 +0200

    Change 11982

--- test.cs
+++ test.cs
@@ -3987,7 +3987,7 @@ namespace Runner
             int i3979 = 11979;
             int i3980 = 11980;
             int i3981 = 11981;
-            int i3982 = 3982;
+            int i3982 = 11982;
             int i3983 = 3983;
             int i3984 = 3984;
             int i3985 = 3985;
