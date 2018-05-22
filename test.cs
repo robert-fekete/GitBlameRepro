b9d07ce052ecd1875d05eaade02d82048671a945
rfe <rfe@phaseone.com>
2018-05-22 11:22:08 +0200

    Change 10705

--- test.cs
+++ test.cs
@@ -2710,7 +2710,7 @@ namespace Runner
             int i2702 = 10702;
             int i2703 = 10703;
             int i2704 = 10704;
-            int i2705 = 2705;
+            int i2705 = 10705;
             int i2706 = 2706;
             int i2707 = 2707;
             int i2708 = 2708;
