d65bd4f1f7bf4ab06666e9151f8dc93d755e205e
rfe <rfe@phaseone.com>
2018-05-22 11:20:39 +0200

    Change 9980

--- test.cs
+++ test.cs
@@ -1985,7 +1985,7 @@ namespace Runner
             int i1977 = 9977;
             int i1978 = 9978;
             int i1979 = 9979;
-            int i1980 = 1980;
+            int i1980 = 9980;
             int i1981 = 1981;
             int i1982 = 1982;
             int i1983 = 1983;
