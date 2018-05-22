7f0afc72b94a6896d62a663525b1afc4a7e2692f
rfe <rfe@phaseone.com>
2018-05-22 11:20:56 +0200

    Change 10130

--- test.cs
+++ test.cs
@@ -2135,7 +2135,7 @@ namespace Runner
             int i2127 = 10127;
             int i2128 = 10128;
             int i2129 = 10129;
-            int i2130 = 2130;
+            int i2130 = 10130;
             int i2131 = 2131;
             int i2132 = 2132;
             int i2133 = 2133;
