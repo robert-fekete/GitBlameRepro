ae200b059f49726c873de85cb57bb32f45a38de2
rfe <rfe@phaseone.com>
2018-05-22 11:20:47 +0200

    Change 10059

--- test.cs
+++ test.cs
@@ -2064,7 +2064,7 @@ namespace Runner
             int i2056 = 10056;
             int i2057 = 10057;
             int i2058 = 10058;
-            int i2059 = 2059;
+            int i2059 = 10059;
             int i2060 = 2060;
             int i2061 = 2061;
             int i2062 = 2062;
