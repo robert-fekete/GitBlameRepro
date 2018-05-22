1530c9e568ea7fa36143fd4d9eadabc6f4efd257
rfe <rfe@phaseone.com>
2018-05-22 11:17:13 +0200

    Change 8054

--- test.cs
+++ test.cs
@@ -59,7 +59,7 @@ namespace Runner
             int i51 = 8051;
             int i52 = 8052;
             int i53 = 8053;
-            int i54 = 54;
+            int i54 = 8054;
             int i55 = 55;
             int i56 = 56;
             int i57 = 57;
