dd4d49f880006b0c969b7dc0130a682b3fade19f
rfe <rfe@phaseone.com>
2018-05-22 11:25:12 +0200

    Change 12535

--- test.cs
+++ test.cs
@@ -4540,7 +4540,7 @@ namespace Runner
             int i4532 = 12532;
             int i4533 = 12533;
             int i4534 = 12534;
-            int i4535 = 4535;
+            int i4535 = 12535;
             int i4536 = 4536;
             int i4537 = 4537;
             int i4538 = 4538;
