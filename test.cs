9524349a6ff6a40a12e8a204ae6f195ed57b59ea
rfe <rfe@phaseone.com>
2018-05-22 11:19:11 +0200

    Change 9155

--- test.cs
+++ test.cs
@@ -1160,7 +1160,7 @@ namespace Runner
             int i1152 = 9152;
             int i1153 = 9153;
             int i1154 = 9154;
-            int i1155 = 1155;
+            int i1155 = 9155;
             int i1156 = 1156;
             int i1157 = 1157;
             int i1158 = 1158;
