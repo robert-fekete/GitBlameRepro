d70e89e07f000ab06d2ba1708b2933982659d979
rfe <rfe@phaseone.com>
2018-05-22 11:21:23 +0200

    Change 10343

--- test.cs
+++ test.cs
@@ -2348,7 +2348,7 @@ namespace Runner
             int i2340 = 10340;
             int i2341 = 10341;
             int i2342 = 10342;
-            int i2343 = 2343;
+            int i2343 = 10343;
             int i2344 = 2344;
             int i2345 = 2345;
             int i2346 = 2346;
