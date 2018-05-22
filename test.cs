9256e219cf70af6af1f749d78fe9587de02b9cb1
rfe <rfe@phaseone.com>
2018-05-22 11:20:01 +0200

    Change 9623

--- test.cs
+++ test.cs
@@ -1628,7 +1628,7 @@ namespace Runner
             int i1620 = 9620;
             int i1621 = 9621;
             int i1622 = 9622;
-            int i1623 = 1623;
+            int i1623 = 9623;
             int i1624 = 1624;
             int i1625 = 1625;
             int i1626 = 1626;
