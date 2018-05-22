29764d55495cf4c9e8b34190b8ad29c7d9de468a
rfe <rfe@phaseone.com>
2018-05-22 11:20:40 +0200

    Change 9996

--- test.cs
+++ test.cs
@@ -2001,7 +2001,7 @@ namespace Runner
             int i1993 = 9993;
             int i1994 = 9994;
             int i1995 = 9995;
-            int i1996 = 1996;
+            int i1996 = 9996;
             int i1997 = 1997;
             int i1998 = 1998;
             int i1999 = 1999;
