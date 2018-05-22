6fa49786920a152381b3f0f6150fd14be8dca717
rfe <rfe@phaseone.com>
2018-05-22 11:22:26 +0200

    Change 10873

--- test.cs
+++ test.cs
@@ -2878,7 +2878,7 @@ namespace Runner
             int i2870 = 10870;
             int i2871 = 10871;
             int i2872 = 10872;
-            int i2873 = 2873;
+            int i2873 = 10873;
             int i2874 = 2874;
             int i2875 = 2875;
             int i2876 = 2876;
