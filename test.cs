4f202c6739311dc6283f1ecd80eee7cddc2af365
rfe <rfe@phaseone.com>
2018-05-22 11:25:26 +0200

    Change 12672

--- test.cs
+++ test.cs
@@ -4677,7 +4677,7 @@ namespace Runner
             int i4669 = 12669;
             int i4670 = 12670;
             int i4671 = 12671;
-            int i4672 = 4672;
+            int i4672 = 12672;
             int i4673 = 4673;
             int i4674 = 4674;
             int i4675 = 4675;
