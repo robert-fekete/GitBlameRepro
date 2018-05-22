69366be587d553656ccb5013618f1ed160a89883
rfe <rfe@phaseone.com>
2018-05-22 11:17:45 +0200

    Change 8356

--- test.cs
+++ test.cs
@@ -361,7 +361,7 @@ namespace Runner
             int i353 = 8353;
             int i354 = 8354;
             int i355 = 8355;
-            int i356 = 356;
+            int i356 = 8356;
             int i357 = 357;
             int i358 = 358;
             int i359 = 359;
