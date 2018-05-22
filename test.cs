92cd6db90488e3dda26cb15bdf492f8e65975728
rfe <rfe@phaseone.com>
2018-05-22 11:19:51 +0200

    Change 9530

--- test.cs
+++ test.cs
@@ -1535,7 +1535,7 @@ namespace Runner
             int i1527 = 9527;
             int i1528 = 9528;
             int i1529 = 9529;
-            int i1530 = 1530;
+            int i1530 = 9530;
             int i1531 = 1531;
             int i1532 = 1532;
             int i1533 = 1533;
