2ca45c42222c4a5db94d874bff86d17d1fa71369
rfe <rfe@phaseone.com>
2018-05-22 11:26:50 +0200

    Change 13528

--- test.cs
+++ test.cs
@@ -5533,7 +5533,7 @@ namespace Runner
             int i5525 = 13525;
             int i5526 = 13526;
             int i5527 = 13527;
-            int i5528 = 5528;
+            int i5528 = 13528;
             int i5529 = 5529;
             int i5530 = 5530;
             int i5531 = 5531;
