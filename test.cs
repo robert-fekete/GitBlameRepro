b746ed1d9db62ce7c64903630e4e92389eb54695
rfe <rfe@phaseone.com>
2018-05-22 11:17:19 +0200

    Change 8105

--- test.cs
+++ test.cs
@@ -110,7 +110,7 @@ namespace Runner
             int i102 = 8102;
             int i103 = 8103;
             int i104 = 8104;
-            int i105 = 105;
+            int i105 = 8105;
             int i106 = 106;
             int i107 = 107;
             int i108 = 108;
