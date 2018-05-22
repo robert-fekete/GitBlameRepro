f11cdd63873e731c130cbd3a6a6563daa528f099
rfe <rfe@phaseone.com>
2018-05-22 11:20:29 +0200

    Change 9887

--- test.cs
+++ test.cs
@@ -1892,7 +1892,7 @@ namespace Runner
             int i1884 = 9884;
             int i1885 = 9885;
             int i1886 = 9886;
-            int i1887 = 1887;
+            int i1887 = 9887;
             int i1888 = 1888;
             int i1889 = 1889;
             int i1890 = 1890;
