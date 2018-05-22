8f8be8fd573cefe34f5821ec44c1bc990f61aef9
rfe <rfe@phaseone.com>
2018-05-22 11:17:35 +0200

    Change 8262

--- test.cs
+++ test.cs
@@ -267,7 +267,7 @@ namespace Runner
             int i259 = 8259;
             int i260 = 8260;
             int i261 = 8261;
-            int i262 = 262;
+            int i262 = 8262;
             int i263 = 263;
             int i264 = 264;
             int i265 = 265;
