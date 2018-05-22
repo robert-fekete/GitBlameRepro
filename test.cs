9a2bd6e82da4d04372d50806414045f1bb73e05c
rfe <rfe@phaseone.com>
2018-05-22 11:24:43 +0200

    Change 12240

--- test.cs
+++ test.cs
@@ -4245,7 +4245,7 @@ namespace Runner
             int i4237 = 12237;
             int i4238 = 12238;
             int i4239 = 12239;
-            int i4240 = 4240;
+            int i4240 = 12240;
             int i4241 = 4241;
             int i4242 = 4242;
             int i4243 = 4243;
