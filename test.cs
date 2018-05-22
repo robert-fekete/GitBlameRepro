0c5cfcff3e8d7314b513142d394ebb8a8ed42ca2
rfe <rfe@phaseone.com>
2018-05-22 11:22:10 +0200

    Change 10723

--- test.cs
+++ test.cs
@@ -2728,7 +2728,7 @@ namespace Runner
             int i2720 = 10720;
             int i2721 = 10721;
             int i2722 = 10722;
-            int i2723 = 2723;
+            int i2723 = 10723;
             int i2724 = 2724;
             int i2725 = 2725;
             int i2726 = 2726;
