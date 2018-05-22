8429316582f739f0eac7c0af5b9da083fa36f708
rfe <rfe@phaseone.com>
2018-05-22 11:19:28 +0200

    Change 9304

--- test.cs
+++ test.cs
@@ -1309,7 +1309,7 @@ namespace Runner
             int i1301 = 9301;
             int i1302 = 9302;
             int i1303 = 9303;
-            int i1304 = 1304;
+            int i1304 = 9304;
             int i1305 = 1305;
             int i1306 = 1306;
             int i1307 = 1307;
