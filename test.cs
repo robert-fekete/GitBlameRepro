446285c52c549056ad9ff74340fb60ada8b65049
rfe <rfe@phaseone.com>
2018-05-22 11:17:10 +0200

    Change 8019

--- test.cs
+++ test.cs
@@ -24,7 +24,7 @@ namespace Runner
             int i16 = 8016;
             int i17 = 8017;
             int i18 = 8018;
-            int i19 = 19;
+            int i19 = 8019;
             int i20 = 20;
             int i21 = 21;
             int i22 = 22;
