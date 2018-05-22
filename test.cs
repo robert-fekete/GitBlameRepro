8e70ec15186cf309c27048a895b7b2a234c373aa
rfe <rfe@phaseone.com>
2018-05-22 11:20:16 +0200

    Change 9773

--- test.cs
+++ test.cs
@@ -1778,7 +1778,7 @@ namespace Runner
             int i1770 = 9770;
             int i1771 = 9771;
             int i1772 = 9772;
-            int i1773 = 1773;
+            int i1773 = 9773;
             int i1774 = 1774;
             int i1775 = 1775;
             int i1776 = 1776;
