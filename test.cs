21ca1103cb0a577e3441994664de85ea0af05bc6
rfe <rfe@phaseone.com>
2018-05-22 11:20:34 +0200

    Change 9932

--- test.cs
+++ test.cs
@@ -1937,7 +1937,7 @@ namespace Runner
             int i1929 = 9929;
             int i1930 = 9930;
             int i1931 = 9931;
-            int i1932 = 1932;
+            int i1932 = 9932;
             int i1933 = 1933;
             int i1934 = 1934;
             int i1935 = 1935;
