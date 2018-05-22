4fa4a5c8a697aa7d08a9117b43d74c7cc7fdc802
rfe <rfe@phaseone.com>
2018-05-22 11:24:25 +0200

    Change 12052

--- test.cs
+++ test.cs
@@ -4057,7 +4057,7 @@ namespace Runner
             int i4049 = 12049;
             int i4050 = 12050;
             int i4051 = 12051;
-            int i4052 = 4052;
+            int i4052 = 12052;
             int i4053 = 4053;
             int i4054 = 4054;
             int i4055 = 4055;
