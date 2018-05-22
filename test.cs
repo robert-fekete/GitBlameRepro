e1020885a16bbbe0fc7ad77751742eba822636de
rfe <rfe@phaseone.com>
2018-05-22 11:18:49 +0200

    Change 8952

--- test.cs
+++ test.cs
@@ -957,7 +957,7 @@ namespace Runner
             int i949 = 8949;
             int i950 = 8950;
             int i951 = 8951;
-            int i952 = 952;
+            int i952 = 8952;
             int i953 = 953;
             int i954 = 954;
             int i955 = 955;
