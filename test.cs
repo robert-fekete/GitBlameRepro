62bc2cd1f8c2dbcb1852feae6d90bac54c7297a1
rfe <rfe@phaseone.com>
2018-05-22 11:20:45 +0200

    Change 10039

--- test.cs
+++ test.cs
@@ -2044,7 +2044,7 @@ namespace Runner
             int i2036 = 10036;
             int i2037 = 10037;
             int i2038 = 10038;
-            int i2039 = 2039;
+            int i2039 = 10039;
             int i2040 = 2040;
             int i2041 = 2041;
             int i2042 = 2042;
