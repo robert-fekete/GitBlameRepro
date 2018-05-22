55a787b83d0ee5eb6746420d9db30cb6608a6c57
rfe <rfe@phaseone.com>
2018-05-22 11:25:49 +0200

    Change 12907

--- test.cs
+++ test.cs
@@ -4912,7 +4912,7 @@ namespace Runner
             int i4904 = 12904;
             int i4905 = 12905;
             int i4906 = 12906;
-            int i4907 = 4907;
+            int i4907 = 12907;
             int i4908 = 4908;
             int i4909 = 4909;
             int i4910 = 4910;
