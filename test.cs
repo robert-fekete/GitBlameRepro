4beb66e35394fa464a847afa7da323892003f192
rfe <rfe@phaseone.com>
2018-05-22 11:18:05 +0200

    Change 8534

--- test.cs
+++ test.cs
@@ -539,7 +539,7 @@ namespace Runner
             int i531 = 8531;
             int i532 = 8532;
             int i533 = 8533;
-            int i534 = 534;
+            int i534 = 8534;
             int i535 = 535;
             int i536 = 536;
             int i537 = 537;
