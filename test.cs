0ca211d21798b827b0f57f805cff334ebf069c2d
rfe <rfe@phaseone.com>
2018-05-22 11:18:16 +0200

    Change 8636

--- test.cs
+++ test.cs
@@ -641,7 +641,7 @@ namespace Runner
             int i633 = 8633;
             int i634 = 8634;
             int i635 = 8635;
-            int i636 = 636;
+            int i636 = 8636;
             int i637 = 637;
             int i638 = 638;
             int i639 = 639;
