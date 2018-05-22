c573e193d0c0dda57f2a5e32508f6fcafa374ed6
rfe <rfe@phaseone.com>
2018-05-22 11:28:49 +0200

    Change 14708

--- test.cs
+++ test.cs
@@ -6713,7 +6713,7 @@ namespace Runner
             int i6705 = 14705;
             int i6706 = 14706;
             int i6707 = 14707;
-            int i6708 = 6708;
+            int i6708 = 14708;
             int i6709 = 6709;
             int i6710 = 6710;
             int i6711 = 6711;
