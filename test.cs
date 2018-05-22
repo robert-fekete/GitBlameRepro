7a00eb0255f9a66e6cf91978a019ba1493b886c2
rfe <rfe@phaseone.com>
2018-05-22 11:25:51 +0200

    Change 12922

--- test.cs
+++ test.cs
@@ -4927,7 +4927,7 @@ namespace Runner
             int i4919 = 12919;
             int i4920 = 12920;
             int i4921 = 12921;
-            int i4922 = 4922;
+            int i4922 = 12922;
             int i4923 = 4923;
             int i4924 = 4924;
             int i4925 = 4925;
